7bcf5a4e1042202a0f53a58c7ea4d529a9a54b09
KleberDarlan <kleberdarlanmonte@gmail.com>
2015-11-27 00:45:31 -0300

    Criando função para retornar conjunto de assentos contínuos livres, dada uma fileira

--- src/funcoes/companhia.scala
+++ src/funcoes/companhia.scala
@@ -20,20 +20,27 @@ object companhia extends App {
   var assentosPorFila = (lista:List[Int]) => (flag:Int) => lista.filter(_ == flag).size
   
   // 1) C
+  // funcao para retornar conjundo com assentos contínuos livres
   def assentosConinuosLivres(fila: List[Int]) = {
-    fila.map(x => {
-		if (x == 0)
-			x
-	    })
+    var assentos = fila.zipWithIndex.filter(_._1 == 0).map(_._2)
+    var assentosLivres:List[Int] = Nil
+      
+    assentos.sliding(2).foreach(x => {
+      if(x(1) == x(0) + 1)
+        x.foreach { x => assentosLivres = x::assentosLivres }
+    })
+    
+    assentosLivres.distinct.sorted
   }
   
   var aeronave = List(
-              List(0, 0, 1, 1, 0, 1),
+              List(0, 0, 1, 0, 0, 1),
               List(1, 0, 0, 0, 1, 0)
            )
   
   println("Qtd. de assentos livres da Aeronave: " + assentosLivresAeronave(aeronave)(assentosPorFila))  
   println("Qtd. de assentos ocupados da fila: " + assentosPorFila(aeronave(0))(1))
   println("Assentos continuos livres de uma fila: " + assentosConinuosLivres(aeronave(0)))
+//  assentosConinuosLivres(aeronave(0))
   
 }
