41d0a765a479169a6a28851e8491a6fa98311663
KleberDarlan <kleberdarlanmonte@gmail.com>
2015-11-26 23:23:31 -0300

    Refatorando funcoes

--- src/funcoes/companhia.scala
+++ src/funcoes/companhia.scala
@@ -1,11 +1,11 @@
 package funcoes
 
 object companhia extends App {
-  
+
+  // 1) A
   // funcao que retorna assentos livres de uma aeronave
   // passando uma aeronave e uma funcao por parametro
-  
-  def assentosLivres(aeronave:List[List[Int]])(f:(List[Int]) => (Int) => Int):Int = {
+  def assentosLivresAeronave(aeronave:List[List[Int]]) (f:(List[Int]) => (Int) => Int):Int = {
     var resultado = 0
     aeronave.foreach( x => {
       resultado += f(x)(0)
@@ -13,27 +13,27 @@ object companhia extends App {
     resultado
   }
   
-  //  funcao para retornar numero de assentos livres/ocupados
-  //  passando uma fila de assentos por parâmetro e uma flag
-  //  com o valor desejado
+  // 1) B
+  // funcao para retornar numero de assentos livres/ocupados
+  // passando uma fila de assentos por parametro e uma flag
+  // com o valor desejado
+  var assentosPorFila = (lista:List[Int]) => (flag:Int) => lista.filter(_ == flag).size
   
-  var assentosPorFila = (lista:List[Int]) => (flag:Int) => {
-    var resultado = 0
-    lista.foreach( x => {
-        if(x == flag)
-          resultado += 1
-    })
-    resultado
+  // 1) C
+  def assentosConinuosLivres(fila: List[Int]) = {
+    fila.map(x => {
+		if (x == 0)
+			x
+	    })
   }
   
   var aeronave = List(
-              List(0, 0, 1),
-              List(1, 0, 1)
+              List(0, 0, 1, 1, 0, 1),
+              List(1, 0, 0, 0, 1, 0)
            )
   
-  // 1) A - Assentos livres de uma aeronave
-  println("Qtd. de assentos livres da Aeronave: " + assentosLivres(aeronave)(assentosPorFila))
-  // 1) B - Assentos ocupados, dada uma fila  
+  println("Qtd. de assentos livres da Aeronave: " + assentosLivresAeronave(aeronave)(assentosPorFila))  
   println("Qtd. de assentos ocupados da fila: " + assentosPorFila(aeronave(0))(1))
+  println("Assentos continuos livres de uma fila: " + assentosConinuosLivres(aeronave(0)))
   
 }
